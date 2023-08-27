851f9a02f1c47c2f03e0d244ebc4b2e749bc331b
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-08-27 20:03:17 +0500

    client almost complete

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -16,27 +16,27 @@ import roadmunk from "../assets/partners/roadmunk.svg";
 
 const Clients = () => {
   return (
-    <section className="clientbg text-textPrimary text-sm py-6 px-12">
-      <div className="grid grid-cols-4 grid-rows-10 gap-y-5 gap-x-2 md:grid-cols-12 md:grid-rows-5">
+    <section className="w-full clientbg text-textPrimary text-sm py-32 xl:py-52 px-12">
+      <div className="w-full grid grid-cols-4 grid-rows-10 gap-y-5 md:gap-y-16 gap-x-2 md:grid-cols-12 md:grid-flow-row">
         <div
-          className="col-span-4 row-span-1 col-start-1 md:col-start-2 md:col-span-12 md:row-span-7
-        md:inline-grid md:grid-cols-12 md:grid-rows-1"
+          className="w-full col-span-4 row-span-1 col-start-1 md:col-start-2 md:col-span-11 md:row-span-1
+        inline-grid md:grid-cols-12 md:grid-rows-1"
         >
-          <h1 className="md:col-span-7 text-2xl font-normal uppercase">
+          <h1 className="col-span-4 md:col-span-7 md:row-span-1 text-4xl font-normal uppercase">
             Clients & Partners
           </h1>
-          <p className="md:col-span-5 text-justify text-xs py-4 font-extralight">
+          <p className="col-span-4 md:col-span-5 md:row-span-1 text-justify text-xs mt-2 font-extralight">
             The customer remains at the center of our concerns, becoming an
             actor of his success is our goal.
           </p>
         </div>
 
-        <div className="col-span-2 row-span-1 col-start-1">
+        <div className="col-span-2 row-span-1 col-start-1 md:col-start-2">
           <h1 className="text-sm">&#9679;&emsp;Clients</h1>
         </div>
 
-        <div className="col-start-1 col-span-4">
-          <div className="grid grid-cols-2 gap-10">
+        <div className="col-start-1 col-span-4 row-span-3 inline-grid md:row-span-1 md:col-start-2 md:col-span-12 md:grid-cols-12 md:grid-rows-1">
+          <div className="col-span-4 row-span-3 md:row-span-1 grid-cols-2 inline-grid md:col-start-2 md:col-span-12 md:grid-cols-6 md:grid-rows-1 gap-10">
             <div>
               <Image
                 className="mt-6"
@@ -82,12 +82,12 @@ const Clients = () => {
           </div>
         </div>
 
-        <div className="col-span-2 row-span-1 col-start-1">
+        <div className="col-span-2 row-span-1 col-start-1 md:col-start-2">
           <h1 className="text-sm">&#9679;&emsp;Partners</h1>
         </div>
 
-        <div className="col-start-1 col-span-4">
-          <div className="grid grid-cols-2 gap-10">
+        <div className="col-start-1 col-span-4 row-span-3 inline-grid md:row-span-1 md:col-start-2 md:col-span-12 md:grid-cols-12 md:grid-rows-1">
+          <div className="col-span-4 row-span-3 md:row-span-1 grid-cols-2 inline-grid md:col-start-2 md:col-span-12 md:grid-cols-6 md:grid-rows-1 gap-10">
             <div>
               <Image
                 className="row-start-1 mt-3"
