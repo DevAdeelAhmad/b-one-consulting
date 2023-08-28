64a9a3637c6224679fa3ceb1be06aaa01976535f
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-08-28 06:39:26 +0500

    working on About 1

--- components/About/About1.tsx
+++ components/About/About1.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import brandandmark from "@/assets/bg/markandbrand.svg";
 
 const About1 = () => {
   return (
-    <div>About1</div>
-  )
-}
+    <section className="w-full about-1 h-[100vh] flex items-center justify-center bg-black text-textPrimary text-sm py-10 px-12">
+      <div className="grid grid-cols-4 grid-flow-row text-sm md:gap-y-16 md:grid-cols-12 ">
+        <div className="col-start-1 col-span-4">
+          <Image
+            className=" justify-center items-center pb-10"
+            src={brandandmark}
+            alt=""
+            width={280}
+            height={280}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About1
+export default About1;
