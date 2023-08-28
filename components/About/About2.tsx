798cf474c54da27d5fa3c30b4b56eb8df57f0796
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-08-28 05:59:40 +0500

    About 2 Completed

--- components/About/About2.tsx
+++ components/About/About2.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const About2 = () => {
+const About3 = () => {
   return (
-    <div>About2</div>
-  )
-}
+    <section className="w-full about2half text-textPrimary text-[10px] py-0 px-12">
+      <div className="grid grid-cols-4 grid-rows-1 h-[50vh] items-center md:grid-cols-12 about2">
+        <p className="col-start-1 md:col-start-2 col-span-3 md:col-span-5 uppercase">
+          We are a team made up of people from <i>different backgrounds and we are
+          passionate</i>about what we do. Our strength lies in <i >the diversity of
+          each person’s background.</i> We are constantly adapting to <i>new trends and
+          the needs of our customers.</i>
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default About2
+export default About3;
