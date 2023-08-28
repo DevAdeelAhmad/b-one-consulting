ffe780328128ceff0c9c75adaa0de2ff1068fb05
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-08-29 00:30:44 +0500

    working on intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -8,8 +8,7 @@ const Intro = () => {
   return (
     <section className="w-full introbg h-[100vh] px-5 py-5 md:px-10 text-sm md:py-10 bg-[#101010]">
       <div className="grid grid-cols-4 md:grid-cols-12">
-
-        <div className="col-span-4 md:col-span-11 inline-grid grid-cols-4 md:grid-cols-12 gap-5">
+        <div className="col-span-4 md:col-span-12 inline-grid grid-cols-4 md:grid-cols-12 gap-5">
           <div className="col-span-2 col-start-1">
             <Image
               className="pt-2 md:pt-0"
@@ -20,11 +19,8 @@ const Intro = () => {
             />
           </div>
 
-          <div className="col-start-3 md:col-start-11 md:ml-10 lg:col-start-12 col-span-1">
-            <button
-              className="border-[1px] rounded-xl w-32 h-7 border-[#2C33D6] text-[#2C33D6]
-              hover:border-textPrimary hover:bg-[#2C33D6] hover:text-white"
-            >
+          <div className="col-start-3 md:col-start-11 xl:col-start-11 col-span-1 lg:ml-10">
+            <button className="border-[1px] items-center justify-center rounded-xl w-36 h-8 border-[#2C33D6] text-[#2C33D6] hover:bg-[#2C33D6] hover:text-white">
               SAY- HELLO
             </button>
           </div>
@@ -38,27 +34,28 @@ const Intro = () => {
           </div>
         </div>
 
-        <div className="col-span-10 row-span-3 md:col-span-12 inline-grid grid-flow-row grid-cols-4
-        md:grid-cols-12 gap-6 text-center py-40">
-          <div className="col-span-4 md:col-start-3 row-span-1 row-start-1">
+        <div
+          className="col-span-10 row-span-3 py-64 md:col-span-12 inline-grid grid-flow-row grid-cols-4
+        md:grid-cols-12 gap-1 md:gap-6 md:py-40 px-6 md:px-0"
+        >
+          <div className="col-span-4 md:col-start-2 lg:col-start-3 xl:col-start-4 row-span-1 row-start-1">
             <h1 className="text-4xl text-left md:text-5xl uppercase text-textPrimary">
               Together
             </h1>
           </div>
-          <div className="col-span-10 md:col-span-7 md:col-start-5 row-span-1 row-start-2">
+          <div className="col-span-10 md:col-span-7 md:col-start-5 xl:col-start-6 row-span-1 row-start-2">
             <h1 className="text-4xl md:text-5xl text-left uppercase text-textPrimary">
               {`Let's Reach your goals!`}
             </h1>
           </div>
-          <div className="col-span-5 text-left md:text-right md:col-start-5 row-span-1 row-start-3">
-            <p className="text-xs uppercase text-textPrimary">
+          <div className="col-span-3 py-5 md:py-0 md:col-span-5 text-left md:text-right md:col-start-5 row-span-1 row-start-3">
+            <p className="text-xs text-textPrimary">
               Quanta autem vis amicitiae sit, ex hoc intellegi maxime
             </p>
           </div>
         </div>
 
-
-        <div className="col-span-4 row-span-1 md:col-span-11 inline-grid grid-cols-4 md:grid-cols-12 gap-5">
+        <div className="col-span-4 hidden lg:inline-grid row-span-1 md:col-span-11  grid-cols-4 md:grid-cols-12 gap-5">
           <div className="col-span-2 col-start-1">
             <Link href="#contact">
               <p className="text-textPrimary text-xs inline">
