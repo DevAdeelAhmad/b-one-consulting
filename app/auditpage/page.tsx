ebd6b0f2ec9688c48c7d762748f8a7b0b44af481
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-08-31 22:16:20 +0500

    working on audit page

---
+++ app/auditpage/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const auditpage = () => {
+  return (
+    <section className="w-full bg-[#101010] text-textPrimary text-sm pb-10 pt-4 px-12">
+      {/* main div */}
+      <div className="w-full grid grid-cols-4 grid-rows-1 md:gap-y-16 md:grid-cols-12 md:grid-flow-row pb-20">
+        {/* hadding div */}
+        <div className="">
+          <h1>Why conduct an audit?</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default auditpage;
