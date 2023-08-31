ab8300c4ca77ad9ccab036518527ed5f9dfa7616
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-01 02:09:19 +0500

    working on services

--- app/datasol/page.tsx
+++ app/datasol/page.tsx
@@ -6,11 +6,11 @@ import Image from "next/image";
 const page = () => {
   return (
     <section className="w-full bg-[#101010] text-textPrimary text-sm pb-10 pt-4 px-12">
-      <div className="w-full grid grid-cols-4 grid-rows-14 gap-y-8 gap-x-3 md:gap-y-10 md:grid-cols-12 md:grid-flow-row pb-20">
-        <div className="col-start-2 col-span-2 row-start-1">
-          <h3 className="uppercase ">Managing Your Data for Success</h3>
+      <div className="w-full grid grid-cols-4 grid-rows-14 gap-y-8 gap-x-3 md:gap-y-10 md:grid-cols-12 md:grid-rows-12 pb-20">
+        <div className="col-start-2 col-span-2 row-start-1 md:col-start-3 md:col-span-4">
+          <h3 className="uppercase md:text-2xl ">Managing Your Data for Success</h3>
         </div>
-        <div className="col-start-1 col-span-4 row-start-2">
+        <div className="col-start-1 col-span-4 row-start-2 md:col-start-2 md:col-span-8">
           <p>
             Data management encompasses the processes, policies, and tools
             necessary for effective data management within an organization. In
@@ -19,47 +19,47 @@ const page = () => {
             gain a competitive advantage.
           </p>
         </div>
-        <div className="col-start-1 col-span-4 row-start-3">
+        <div className="col-start-1 col-span-4 row-start-3 md:col-start-2 md:col-span-6">
           <p className="uppercase font-thin">The Benefits of Data Management</p>
         </div>
-        <div className="col-start-1 col-span-2 row-start-4">
+        <div className="col-start-1 col-span-2 row-start-4 md:col-start-2 md:col-span-2">
           <p className="font-bold"> Informed Decision Making</p>
           <p>
             Well-managed data provides decision-makers with accurate and
             reliable information to make informed decisions.
           </p>
         </div>
-        <div className="col-start-3 col-span-2 row-start-4">
+        <div className="col-start-3 col-span-2 row-start-4 md:col-start-4 md:col-span-2 md:row-start-4">
           <p className="font-bold"> Competitiveness</p>
           <p>
             Companies that fully leverage their data and use it for innovation
             can gain a competitive edge in the market.
           </p>
         </div>
-        <div className="col-start-1 col-span-4 row-start-5">
-          <Image src={innerser1} alt="" width={350} height={100} />
+        <div className="col-start-1 col-span-4 row-start-5 md:col-start-6 md:col-span-4 md:row-start-4 md:pt-10">
+          <Image src={innerser1} alt="" width={200} height={230} />
         </div>
 
-        <div className="col-start-1 col-span-2 row-start-6">
+        <div className="col-start-1 col-span-2 row-start-6 md:col-start-2 md:col-span-2 md:row-start-4 md:pt-40">
           <p className="font-bold">Cost Efficiency</p>
           <p>
             Efficient data management reduces costs associated with poor data
             quality, processing errors, and non-compliance.
           </p>
         </div>
-        <div className="col-start-3 col-span-2 row-start-6">
+        <div className="col-start-3 col-span-2 row-start-6 md:col-start-4 md:col-span-2 md:row-start-4 md:pt-40">
           <p className="font-bold">Regulatory Compliance</p>
           <p>
             Data management helps comply with legal and regulatory requirements
             regarding data protection and privacy.
           </p>
         </div>
-        <div className="col-start-1 col-span-4 row-start-7">
-          <h3 className="uppercase">
+        <div className="col-start-1 col-span-4 row-start-7 md:col-start-2 md:col-span-4 md:row-start-5 ">
+          <h3 className="uppercase md:text-2xl">
             The power of reporting and data visualization
           </h3>
         </div>
-        <div className="col-start-1 col-span-4 row-start-8">
+        <div className="col-start-1 col-span-4 row-start-8 md:col-start-2 md:col-span-8 md:row-start-6">
           <p>
             In the world of data, it is essential to know how to effectively
             present and communicate key information. Data visualization plays a
@@ -72,35 +72,35 @@ const page = () => {
           </p>
         </div>
 
-        <div className="col-start-1 col-span-4 row-start-11">
+        <div className="col-start-1 col-span-4 row-start-11 md:col-start-2 md:col-span-4 row-start-7 ">
           <p className="uppercase font-thin">The Benefits of Data Management</p>
         </div>
-        <div className="col-start-1 col-span-2 row-start-12">
+        <div className="col-start-1 col-span-2 row-start-12  md:col-start-2 md:col-span-2 md:row-start-8">
           <p className="font-bold"> Informed Decision Making</p>
           <p>
             Well-managed data provides decision-makers with accurate and
             reliable information to make informed decisions.
           </p>
         </div>
-        <div className="col-start-3 col-span-2 row-start-12">
+        <div className="col-start-3 col-span-2 row-start-12 md:col-start-4 md:col-span-2 md:row-start-8">
           <p className="font-bold"> Competitiveness</p>
           <p>
             Companies that fully leverage their data and use it for innovation
             can gain a competitive edge in the market.
           </p>
         </div>
-        <div className="col-start-1 col-span-4 row-start-13">
-          <Image src={innerser2} alt="" width={350} height={100} />
+        <div className="col-start-1 col-span-4 row-start-13 md:col-start-6 md:col-span-4 md:row-start-8">
+          <Image src={innerser2} alt="" width={200} height={230} />
         </div>
 
-        <div className="col-start-1 col-span-2 row-start-14">
+        <div className="col-start-1 col-span-2 row-start-14 md:col-start-2 md:col-span-2 md:row-start-8 ">
           <p className="font-bold">Cost Efficiency</p>
           <p>
             Efficient data management reduces costs associated with poor data
             quality, processing errors, and non-compliance.
           </p>
         </div>
-        <div className="col-start-3 col-span-2 row-start-14">
+        <div className="col-start-3 col-span-2 row-start-14 md:col-start-4 md:col-span-2 md:row-start-8">
           <p className="font-bold">Regulatory Compliance</p>
           <p>
             Data management helps comply with legal and regulatory requirements
