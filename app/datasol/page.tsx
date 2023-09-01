a2dc2b773da5623c8ab7f8cc56ae0da6a87dcfd6
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-01 05:35:02 +0500

    data solution inner page completed

--- app/datasol/page.tsx
+++ app/datasol/page.tsx
@@ -8,7 +8,9 @@ const page = () => {
     <section className="w-full bg-[#101010] text-textPrimary text-sm pb-10 pt-4 px-12">
       <div className="w-full grid grid-cols-4 grid-rows-14 gap-y-8 gap-x-3 md:gap-y-10 md:grid-cols-12 md:grid-rows-12 pb-20">
         <div className="col-start-2 col-span-2 row-start-1 md:col-start-3 md:col-span-4">
-          <h3 className="uppercase md:text-2xl ">Managing Your Data for Success</h3>
+          <h3 className="uppercase md:text-2xl ">
+            Managing Your Data for Success
+          </h3>
         </div>
         <div className="col-start-1 col-span-4 row-start-2 md:col-start-2 md:col-span-8">
           <p>
@@ -81,6 +83,14 @@ const page = () => {
             Well-managed data provides decision-makers with accurate and
             reliable information to make informed decisions.
           </p>
+          
+        <div className="col-start-1 col-span-2 row-start-14 md:col-start-2 md:col-span-2 md:row-start-8 md:pt-10">
+          <p className="font-bold">Cost Efficiency</p>
+          <p>
+            Efficient data management reduces costs associated with poor data
+            quality, processing errors, and non-compliance.
+          </p>
+        </div>
         </div>
         <div className="col-start-3 col-span-2 row-start-12 md:col-start-4 md:col-span-2 md:row-start-8">
           <p className="font-bold"> Competitiveness</p>
@@ -88,25 +98,18 @@ const page = () => {
             Companies that fully leverage their data and use it for innovation
             can gain a competitive edge in the market.
           </p>
-        </div>
-        <div className="col-start-1 col-span-4 row-start-13 md:col-start-6 md:col-span-4 md:row-start-8">
-          <Image src={innerser2} alt="" width={200} height={230} />
-        </div>
-
-        <div className="col-start-1 col-span-2 row-start-14 md:col-start-2 md:col-span-2 md:row-start-8 ">
-          <p className="font-bold">Cost Efficiency</p>
-          <p>
-            Efficient data management reduces costs associated with poor data
-            quality, processing errors, and non-compliance.
-          </p>
-        </div>
-        <div className="col-start-3 col-span-2 row-start-14 md:col-start-4 md:col-span-2 md:row-start-8">
+          <div className="col-start-3 col-span-2 row-start-14 md:col-start-4 md:col-span-2 md:row-start-8 md:pt-10">
           <p className="font-bold">Regulatory Compliance</p>
           <p>
             Data management helps comply with legal and regulatory requirements
             regarding data protection and privacy.
           </p>
         </div>
+        </div>
+        <div className="col-start-1 col-span-4 row-start-13 md:col-start-6 md:col-span-4 md:row-start-8 md:pt-10">
+          <Image src={innerser2} alt="" width={200} height={230} />
+        </div>
+
       </div>
     </section>
   );
