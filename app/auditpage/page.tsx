6429c121acd617510fc672044e47a577b558a956
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-09-01 06:36:20 +0500

    audit fix

--- app/auditpage/page.tsx
+++ app/auditpage/page.tsx
@@ -3,14 +3,14 @@ import React from "react";
 const auditpage = () => {
   return (
     <section className="w-full bg-[#101010] text-textPrimary text-sm pb-10 pt-4 px-12">
-      {/* main div */}
-      <div className="w-full grid grid-cols-4 grid-rows-12 gap-y-5 md:gap-y-10 md:grid-cols-12 md:grid-flow-row pb-20">
-        {/* hadding div */}
+
+      <div className="w-full grid grid-cols-4 grid-flow-row gap-y-5 md:gap-y-10 md:grid-cols-12 md:grid-flow-row pb-20">
+
         <div className="col-start-1 col-span-3 row-start-1 md:col-start-1 md:col-span-6 pt-10">
           <h1 className="md:text-4xl text-xl uppercase">Why conduct an audit?</h1>
         </div>
-        {/* paragraph div */}
-        <div className=" col-start-1 col-span-4 row-start-2 md:row-start-2 md:col-start-1 md:col-span-7">
+
+        <div className=" col-start-1 col-span-4 row-start-2 md:row-start-2 md:col-start-1 md:col-span-12">
           <p className="">
             An audit allows for an assessment of your company's current
             situation. It helps identify areas that require improvements or
@@ -19,23 +19,22 @@ const auditpage = () => {
           </p>
         </div>
 
-        <div className="row-start-3 col-start-1 col-span-3 md:row-start-3 md:col-start-1 md:col-span-4 pt-10">
+        <div className="row-start-3 col-start-1 col-span-3 md:row-start-3 md:col-start-1 md:col-span-4 pt-8">
           <h1 className="md:text-4xl text-xl uppercase">The audit process</h1>
         </div>
 
-        {/* next three similar divs */}
 
-        <div className="row-start-4 col-start-1 col-span-1 md:row-start-4 md:col-start-1 md:col-span-1 rounded-full bg-[#F1F1F1]">
+        <div className="row-start-4 col-start-1 flex items-center justify-center h-24 w-24 col-span-1 md:row-start-4 md:col-start-1 md:col-span-1 rounded-full bg-[#F1F1F1]">
           <p className="text-black flex text-center justify-center md:text-2xl text-xl font-bold">
             1
           </p>
         </div>
         <div className="row-start-5 col-start-1 col-span-3 md:row-start-4 md:col-start-3 md:col-span-3">
-          <p className="md:text-2xl text-lg uppercase">
+          <p className="md:text-xl text-base uppercase">
             Conducting a diagnosis of current processes & information systems.
           </p>
         </div>
-        <div className="row-start-6 col-start-1 col-span-4 md:row-start-4 md:col-start-7 md:col-span-5 pb-10 md:pb-20">
+        <div className="row-start-6 col-start-1 col-span-4 md:row-start-4 md:col-start-7 md:col-span-5 pb-10 md:pb-20 text-sm md:text-base">
           <p>
             The diagnosis of current processes and information systems involves
             evaluating an enterprise's operational processes and technological
@@ -43,20 +42,20 @@ const auditpage = () => {
           </p>
         </div>
 
-        {/* next three similar divs */}
 
-        <div className="row-start-7 col-start-1 col-span-1 md:row-start-5 md:col-start-1 md:col-span-1 rounded-full bg-[#F1F1F1]">
+
+        <div className="row-start-7 col-start-1 flex items-center justify-center w-24 h-24 col-span-1 md:row-start-5 md:col-start-1 md:col-span-1 rounded-full bg-[#F1F1F1]">
           <p className="text-black flex text-center justify-center md:text-2xl text-xl font-bold">
             2
           </p>
         </div>
         <div className="row-start-8 col-start-1 col-span-2 md:row-start-5 md:col-start-3 md:col-span-2">
           <p
-           className="md:text-2xl text-lg uppercase">
+            className="md:text-xl text-base uppercase">
             synthesis and developement of recommendations
           </p>
         </div>
-        <div className="row-start-9 col-start-1 col-span-4 md:row-start-5 md:col-start-7 md:col-span-5 pb-10 md:pb-20">
+        <div className="row-start-9 col-start-1 col-span-4 md:row-start-5 md:col-start-7 md:col-span-5 pb-10 md:pb-20 text-sm md:text-base">
           <p>
             Based on the identification of problems and opportunities,
             recommendations are formulated to address the issues and leverage
@@ -67,17 +66,17 @@ const auditpage = () => {
           </p>
         </div>
 
-        {/* next three similar divs */}
 
-        <div className="row-start-10 col-start-1 col-span-1 md:row-start-6 md:col-start-1 md:col-span-1 rounded-full bg-[#F1F1F1]">
+
+        <div className="row-start-10 col-start-1 w-24 h-24 col-span-1 md:row-start-6 md:col-start-1 md:col-span-1 flex items-center justify-center rounded-full bg-[#F1F1F1]">
           <p className="text-black flex text-center justify-center md:text-2xl text-xl font-bold">
             3
           </p>
         </div>
         <div className="row-start-11 col-start-1 col-span-3 md:row-start-6 md:col-start-3 md:col-span-2">
-          <p className="md:text-2xl text-lg uppercase">implementation and execution </p>
+          <p className="md:text-xl text-base uppercase">implementation and execution </p>
         </div>
-        <div className="row-start-12 col-start-1 col-span-4 md:row-start-6 md:col-start-7 md:col-span-5">
+        <div className="row-start-12 col-start-1 col-span-4 md:row-start-6 md:col-start-7 md:col-span-5 text-sm md:text-base">
           <p>
             Once the recommendations have been communicated, the next step is to
             implement them. This involves planning actions, allocating
