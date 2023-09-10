28f23fa64123f1c8f2804691090ac758149e3487
Adeel Ahmad <devadeelahmad@gmail.com>
2023-09-10 09:10:50 +0500

    Fix: About 1

--- components/About/About1.tsx
+++ components/About/About1.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import About1Img from "@/assets/bg/about4.svg";
+import About1Img from "@/assets/bg/About4.png";
 
 interface ISingleCardProps {
   text: string;
@@ -10,28 +10,28 @@ interface ISingleCardProps {
 const SingleCard = ({ text, desc }: ISingleCardProps) => {
   return (
     <div className="w-40 rounded-[1.75rem] text-center py-[1.75rem] font-prompt">
-      <h1 className="text-xl md:text-3xl font-semibold font-textFamily">{text}</h1>
-      <p className="text-sm font-textFamily">{desc}</p>
+      <h1 className="text-2xl md:text-4xl lg:text-5xl xl:text-6xl font-medium pl-10 md:pl-0">{text}</h1>
+      <p className="text-sm md:text-base font-light pl-10 md:pl-0">{desc}</p>
     </div>
   );
 };
 
 const About1 = () => {
   return (
-    <section className="w-full h-[100vh] text-textPrimary text-sm py-32 px-2 md:px-12">
-      <div className="grid grid-cols-4 md:grid-cols-12 grid-rows-1 gap-y-5 gap-x-2">
-        <div className="col-span-4 md:col-start-1 row-start-1 col-start-1 md:col-span-6 h-3/6 md:h-full">
-          <Image src={About1Img} alt="About 4 Image" width={1000} height={1000} />
+    <section className="w-full h-[100vh] text-textPrimary flex flex-col justify-center text-sm py-14 px-2 md:px-12">
+      <div className="grid grid-cols-4 md:grid-cols-12 grid-flow-row gap-y-5">
+        <div className="col-span-4 md:col-span-6 row-start-1 col-start-1 flex items-center justify-center">
+          <Image className="xl:h-[60vh] xl:w-[60vh]" src={About1Img} alt="About 4 Image" width={400} height={400} />
         </div>
-        <div className="col-span-2 col-start-1 row-start-1 md:col-start-8 md:col-span-3
-        flex flex-col justify-center gap-y-10">
-          <SingleCard text="+10" desc="YEARS" />
-          <SingleCard text="+1000" desc="PROJECTS" />
+        <div className="col-span-1 col-start-1 row-start-1 row-span-1 md:col-start-8 md:col-span-3
+        flex flex-col justify-around md:justify-between">
+          <div><SingleCard text="+10" desc="YEARS" /></div>
+          <div><SingleCard text="+1000" desc="PROJECTS" /></div>
         </div>
-        <div className="col-span-2 col-start-3 row-start-1 md:col-start-10 md:col-span-3
-        flex flex-col justify-center gap-y-10">
-          <SingleCard text="4" desc="CONTENT" />
-          <SingleCard text="100%" desc="OF OUR CUSTOMER SATISFY" />
+        <div className="col-span-1 col-start-3 row-start-1 row-span-1 md:col-start-10 md:col-span-3
+        flex flex-col justify-around md:justify-between items-center">
+          <div><SingleCard text="4" desc="CONTENT" /></div>
+          <div><SingleCard text="100%" desc="OF OUR CUSTOMER SATISFY" /></div>
         </div>
       </div>
     </section>
--- components/About/About4.tsx
+++ components/About/About4.tsx
@@ -4,24 +4,7 @@ interface ISingleCardProps {
   text: string;
   desc : string; 
 }
-interface ISingleCol {
-  title: string;
-  children?: ReactElement<any, any>;
-}
 
-// SingleCol component
-const SingleCol = ({ title, children }: ISingleCol) => {
-  return (
-    <div>
-      <h2 className="text-xl font-bold mb-2 uppercase text-center mb-8">{title}</h2>
-      <div className="flex items-center justify-center gap-8 flex-col">
-        {children}
-      </div>
-    </div>
-  );
-};
-
-// SingleCard component
 const SingleCard = ({ text, desc } :ISingleCardProps) => {
   return (
     <div className="w-40 border border-[#818489] rounded-[1.75rem] bg-black text-center py-[1.75rem] font-textFamily">
@@ -35,16 +18,10 @@ const About4 = () => {
   return (
     <section className="w-full about4 text-textPrimary text-sm py-32 px-2 md:px-12">
       <div className="grid grid-cols-3 gap-y-5 md:gap-y-16 gap-x-2 md:grid-flow-row">
-        <SingleCol title="dubaï" />
-        <SingleCol title="Paris">
-          <>
             <SingleCard text="+10" desc="YEARS" />
-            <SingleCard text="+1000" desc="PROJECTS" />
-            <SingleCard text="4" desc="CONTENT" />
+            <SingleCard text="+" desc="PROJECTS" />
+            <SingleCard text="+1000" desc="CONTENT" />
             <SingleCard text="100%" desc="OF OUR CUSTOMER SATISFY" />
-          </>
-        </SingleCol>
-        <SingleCol title="Bali" />
       </div>
     </section>
   );
