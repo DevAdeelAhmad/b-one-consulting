8742fa93cfcf03010bda6a0997fb5771024b7a9b
Adeel Ahmad <devadeelahmad@gmail.com>
2023-09-10 06:46:38 +0500

    Fix: About 3 Fixed

--- components/About/About3.tsx
+++ components/About/About3.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const About3 = () => {
   return (
-    <section className="w-full h-[60vh] md:h-[100vh] about-3 flex items-center justify-center bg-[#101010] text-textPrimary text-sm py-10 px-12">
+    <section className="w-full h-[100vh] about-3 flex items-center justify-center bg-[#101010] text-textPrimary text-sm py-10 px-12">
       <div className="grid grid-cols-4 grid-flow-row text-sm md:gap-y-16 md:grid-cols-12 ">
         <div className="about3 hidden md:inline-grid md:h-[50vh] col-span-4 col-start-3"></div>
-        <p className="col-span-4 xl:text-lg row-span-1 flex items-center uppercase justify-center text-textPrimary">
+        <p className="col-span-4 xl:text-lg 2xl:text-xl font-Prompt row-span-1 flex items-center uppercase justify-center text-textPrimary">
           Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non
           probes eius, a quo dissentias. quid enim me prohiberet aojaoca^
           Epicureum
