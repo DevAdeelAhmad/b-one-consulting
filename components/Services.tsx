05b76c237a03b898b8deac4794565edcd4257c82
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-11 19:45:17 +0500

    fix

--- components/Services.tsx
+++ components/Services.tsx
@@ -11,7 +11,7 @@ const Services = () => {
       <div className="w-full grid grid-cols-4 grid-flow-row gap-y-16 2xl:gap-y-40 gap-x-20 xl:gap-x-10 2xl:gap-x-5 md:grid-cols-12 pt-4 md:grid-flow-row pb-10">
         <div className="col-start-1 md:col-start-2 col-span-4 mt-4 2xl:mt-2">
           <h2 className="text-3xl xl:text-4xl 2xl:text-5xl text-white font-Titillium font-light uppercase font-headingText">
-            360º Service
+            360º  
           </h2>
         </div>
         <div className="col-start-1 md:col-start-3 h-64 2xl:h-96 col-span-2 flex flex-col justify-between border-l border-solid border-textPrimary">
