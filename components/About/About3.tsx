25911e2e6d6d43561c3cc7e40c6919eb3d42aa45
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-13 02:07:38 +0500

    fixing about 3

--- components/About/About3.tsx
+++ components/About/About3.tsx
@@ -4,7 +4,7 @@ const About3 = () => {
   return (
     <section className="w-full h-[100vh] about-3 flex items-center justify-center bg-[#101010] text-textPrimary text-sm py-10 px-12">
       <div className="grid grid-cols-4 grid-flow-row text-sm md:gap-y-16 md:grid-cols-12 ">
-        <div className="about3 hidden md:inline-grid md:h-[50vh] col-span-4 col-start-3"></div>
+        <div className="about3  md:inline-grid md:h-[50vh] col-span-4 col-start-3"></div>
         <p className="col-span-4 text-md md:text-lg xl:text-2xl font-Prompt row-span-1 flex items-center uppercase justify-center text-textPrimary">
           Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non
           probes eius, a quo dissentias. quid enim me prohiberet aojaoca^
